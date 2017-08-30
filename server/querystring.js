f5f598853374092fbaf0dcfa2e5cf8b2ad42a64b
hns <hunaisong@163.com>
2017-08-30 17:33:12 +0800

    url模块用法

---
+++ server/querystring.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: hunaisong 
+ * @Date: 2017-08-30 17:32:01 
+ * @Last Modified by:   hunaisong 
+ * @Last Modified time: 2017-08-30 17:32:01 
+ */
---
+++ server/url.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: hunaisong 
+ * @Date: 2017-08-30 17:25:02 
+ * @Last Modified by: hunaisong
+ * @Last Modified time: 2017-08-30 17:31:12
+ */
+// 使用node.js的http模块会用到挺多其他工具类的api，比如url
+var url = require("url");
+var str = "http://user:pass@host.com:8080/p/a/t/h?query=string#hash"
+console.log(url.parse(str))
+// 会得到如下的对象
+/*Url {
+    protocol: 'http:',
+    slashes: true,
+    auth: 'user:pass',
+    host: 'host.com:8080',
+    port: '8080',
+    hostname: 'host.com',
+    hash: '#hash',
+    search: '?query=string',
+    query: 'query=string',
+    pathname: '/p/a/t/h',
+    path: '/p/a/t/h?query=string',
+    href: 'http://user:pass@host.com:8080/p/a/t/h?query=string#hash' }*/
+
+// 反过来有一个url.format()方法，将传入的对象转化为url
+
+url.format({
+    protocol: 'http:',
+    host: 'www.example.com',
+    pathname: '/p/a/t/h',
+    search: 'query=string'
+});
+/* =>
+'http://www.example.com/p/a/t/h?query=string'
+*/
+
+// resolve方法，用于拼接
+url.resolve('http://www.example.com/foo/bar', '../baz');
+/* =>
+http://www.example.com/baz
+*/
