5f5065d4749e91bd354959263215fcb93d933440
hns <hunaisong@163.com>
2017-08-31 09:13:22 +0800

    url模块常用api

--- server/url.js
+++ server/url.js
@@ -2,7 +2,7 @@
  * @Author: hunaisong 
  * @Date: 2017-08-30 17:25:02 
  * @Last Modified by: hunaisong
- * @Last Modified time: 2017-08-30 17:31:12
+ * @Last Modified time: 2017-08-31 09:11:43
  */
 // 使用node.js的http模块会用到挺多其他工具类的api，比如url
 var url = require("url");
@@ -25,18 +25,20 @@ console.log(url.parse(str))
 
 // 反过来有一个url.format()方法，将传入的对象转化为url
 
-url.format({
+var url_1 = url.format({
     protocol: 'http:',
     host: 'www.example.com',
     pathname: '/p/a/t/h',
     search: 'query=string'
 });
+console.log(url_1)
 /* =>
 'http://www.example.com/p/a/t/h?query=string'
 */
 
-// resolve方法，用于拼接
-url.resolve('http://www.example.com/foo/bar', '../baz');
+// resolve方法，用于拼接，返回上一级菜单，baz和foo是同级文件夹
+var url_2 = url.resolve('http://www.example.com/foo/bar', '../baz');
+console.log(url_2)
 /* =>
 http://www.example.com/baz
 */
