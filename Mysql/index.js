5cb707ee9f7692104086e4c470cedf3db3bea68e
hns <hunaisong@163.com>
2017-10-09 09:38:45 +0800

    node 连接 mysql

--- Mysql/index.js
+++ Mysql/index.js
@@ -2,7 +2,7 @@
  * @Author: hunaisong 
  * @Date: 2017-09-29 13:26:46 
  * @Last Modified by: hunaisong
- * @Last Modified time: 2017-09-29 13:56:49
+ * @Last Modified time: 2017-09-30 11:17:40
  */
 // 从数据库查询
 var mysql = require('mysql');
@@ -14,8 +14,9 @@ var connection = mysql.createConnection({
     database:'test'
 })
 connection.connect();
+// 创建sql语句
 var sql = 'SELECT * FROM websites';
-
+// 调用sql语句以及一个callback
 connection.query(sql,function (err,results) {
     if (err) {
         console.log('[SELECT ERROR] - ',err.message);
--- Mysql/test.js
+++ Mysql/test.js
@@ -2,21 +2,26 @@
  * @Author: hunaisong 
  * @Date: 2017-09-27 14:40:06 
  * @Last Modified by: hunaisong
- * @Last Modified time: 2017-09-29 13:27:04
+ * @Last Modified time: 2017-09-30 11:15:59
  */
 // 用来测试node连接mysql的
+// 引入mysql依赖
 var mysql = require('mysql');
+// 创建连接的基本配置信息
 var connection = mysql.createConnection({
+    // 主机
     host: 'localhost',
+    // 用户
     user: 'root',
+    // 密码
     password: '123456',
+    // 数据库名称
     database: 'test'
 });
-
+// 连接数据库
 connection.connect();
-
+// 调用数据库库语法语句，然后来一个callback
 connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
 });
-// 测试连接成功
